ea3c08754707d0f6dc2f0b091020b6a26b3331a8
jfanzago <jfanzago@gmail.com>
2016-02-18 17:10:42 -0500

    added description to actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,20 +1,41 @@
 class ProductsController < ApplicationController
 	def index
+		if params[:user_id]
+			@user = User.find_by(id: params[:user_id])
+			@products = @user.products.all
+		else
+			@products = Product.all
+		end
+	def 	def show
 		@user = User.find_by(id: params[:user_id])
-		@products = @user.products.all
-	end
-	def new
-		@user = User.find_by(:id params[:user_id])
-		@products = @user.products.new
-	end		
-	def create 
-		@user = User.find_by(id: params[:user_id])
-		@user.products.new(uer_params)
+		@product = @user.products.find_by(id: params[:id])
+		if @product
+			render 'show'
+		else
+			render 'product_not_found'
+		end
+
+	def destroy
 	end
 
-	private 
+
+	def new
+		@user = User.find_by(id: params[:user_id])
+		@product = @user.products.new
+	end
 	
+	def create
+		@user = User.find_by(id: params[:user_id])
+		@product = @user.products.new(product_params)
+		if @product.save
+			redirect_to user_products_path(@user)
+		else
+			render 'new'
+		end
+
+		private #this says only the three arguments are allowed!
 	def product_params
-		params.require(:product).permit(:title, :description, :deadline)
+		params.require(:product).permit(:title, :description, :amount, :deadline)
+	en
 
 end
