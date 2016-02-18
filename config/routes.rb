08f4dc73c04423fee6bc247f64c3ac5ac234c973
jfanzago <jfanzago@gmail.com>
2016-02-18 16:10:32 -0500

    comment

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 get '/' => 'site#home'
 # get 'users/productid'  => 'products#new', as: new_user_products_path
 
-resources :users, only: [:show, :destroy,  :create, ] do
+resources :users, only: [:show, :destroy, x:create, ] do #shown in user controller
   resources :products, only: [:index, :new, :create, :destroy]
 
 end
