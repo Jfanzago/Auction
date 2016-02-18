d4d35ed69cc1906af9d36042aabf7b706aa6bf59
jfanzago <jfanzago@gmail.com>
2016-02-18 17:13:51 -0500

    actions defined

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,15 +1,44 @@
 class UserController < ApplicationController
 
-def show
-end
- 
-def create
-end
 
-def destroy
-end
+	def index
+		@users = User.all
+			if @users.empty?
+				render 'no_users'
+			else
+				render 'index'
+		end
+	end
 
-def new
-end
+	def show
+		@user = User.find_by(id: params[:id])
+		if @user
+			render 'show'
+		else
+			render 'user_not_found'
+		end
+	end
 
-end
+	def create
+		@user = User.create(user_params)
+			if @user.save
+				link_to user_path(@user)
+			else
+				render 'new'
+			end
+	end
+
+	def destroy
+		User.find(params[:id]).destroy
+		redirect_to users_path
+	end
+
+	def new
+		@user = User.new
+	end
+	
+	private #this says only the three arguments are allowed!
+	def user_params
+		params.require(:user).permit(:name, :email)
+	end
+end	
