00c248b7996e133170e79012538ea8858b5c39ac
jfanzago <jfanzago@gmail.com>
2016-02-18 16:12:52 -0500

    seeded users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user = User.new(name: "Jane", email:"Jane@gmail.com")
+Jane = User.new(name: "Jane", email:"Jane@gmail.com")
+Bobby = User.new(name: "Bobby", email:"bobby@hotmail.com")
+Joe = User.new(name: "Joe", email:"Joe@gmail.com")
+Nizar = User.new(name: "Nizar", email:"Nizar@yahoo.com")
 
 
 user.products.create(title:"Oven" , description:"hot, hot, hot, hot", deadline: date.now+1)
